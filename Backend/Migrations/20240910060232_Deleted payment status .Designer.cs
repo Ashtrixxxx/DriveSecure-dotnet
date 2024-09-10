e331340ba265af32406a49b782208ed71725bf34
Ashwin S <ashwin94429@gmail.com>
2024-09-10 11:42:08 +0530

    Added a dto to handle complex objects

--- Backend/Migrations/20240910060232_Deleted payment status .Designer.cs
+++ Backend/Migrations/20240910060232_Deleted payment status .Designer.cs
@@ -4,6 +4,7 @@ using Backend.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Backend.Migrations
 {
     [DbContext(typeof(DriveDbContext))]
-    partial class DriveDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240910060232_Deleted payment status ")]
+    partial class Deletedpaymentstatus
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -132,9 +135,8 @@ namespace Backend.Migrations
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
-                    b.Property<string>("RCProof")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
+                    b.Property<int>("RCProof")
+                        .HasColumnType("int");
 
                     b.Property<int?>("UserDetailsUserID")
                         .HasColumnType("int");
